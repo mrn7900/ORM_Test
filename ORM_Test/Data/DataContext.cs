3edc7ba3864b09f4627b6632f6a518546a082200
mrn7900 <rahiminiamohammad@gmail.com>
2023-07-12 10:15:17 +0330

    connected with mysql

--- ORM_Test/Data/DataContext.cs
+++ ORM_Test/Data/DataContext.cs
@@ -5,7 +5,14 @@ namespace ORM_Test.Data
 {
     public class DataContext:DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DataContext(DbContextOptions<DataContext> options) 
+        {
+
+        }
         public DbSet<SuperHero> SuperHeroes { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("Server=127.0.0.1;Port=3306;Database=test1;Uid=root;Pwd=12345678;");
+        }
     }
 }
